b8d7a974ced63c54c588b25ef38389f5a74a60ed
darksunlight <darksunlight210@gmail.com>
2021-11-23 17:58:59 -0800

    yep the eslint indent rule is still broken

--- src/types.ts
+++ src/types.ts
@@ -2,123 +2,123 @@ import { Client } from "@/client";
 
 /* eslint-disable no-unused-vars */
 export type CommandExecuteArguments = {
-	line: string[];
-	client: Client;
+    line: string[];
+    client: Client;
 }
 
 export enum AllRoomsSupport {
-	None = 0,
-	Modified = 1,
-	Full = 2
+    None = 0,
+    Modified = 1,
+    Full = 2
 }
 
 export interface Command {
     data: {
         name: string;
         aliases: string[];
-		expectArg?: string;
-		description?: string;
-		allRooms?: AllRoomsSupport;
+        expectArg?: string;
+        description?: string;
+        allRooms?: AllRoomsSupport;
     };
     // eslint-disable-next-line no-unused-vars
     execute(arg: CommandExecuteArguments): void | Promise<void>;
 }
 
 export enum GatewayEvent {
-	WELCOME = 0,
+    WELCOME = 0,
 
-	AUTHENTICATE = 1,
-	AUTHENTICATE_SUCCESS = 2,
-	AUTHENTICATE_ERROR = 3,
+    AUTHENTICATE = 1,
+    AUTHENTICATE_SUCCESS = 2,
+    AUTHENTICATE_ERROR = 3,
 
-	CHANNEL_JOIN = 4,
-	CHANNEL_JOIN_SUCCESS = 5,
-	CHANNEL_JOIN_ERROR = 6,
+    CHANNEL_JOIN = 4,
+    CHANNEL_JOIN_SUCCESS = 5,
+    CHANNEL_JOIN_ERROR = 6,
 
-	MESSAGE_SEND = 7,
-	MESSAGE_SEND_SUCCESS = 8,
-	MESSAGE_SEND_ERROR = 9,
-	MESSAGE_NEW = 10,
+    MESSAGE_SEND = 7,
+    MESSAGE_SEND_SUCCESS = 8,
+    MESSAGE_SEND_ERROR = 9,
+    MESSAGE_NEW = 10,
 
-	GROUP_GET_REQUEST = 11,
-	GROUP_GET_SUCCESS = 12,
+    GROUP_GET_REQUEST = 11,
+    GROUP_GET_SUCCESS = 12,
 
-	ROOMS_GET_REQUEST = 13,
-	ROOMS_GET_SUCCESS = 14,
+    ROOMS_GET_REQUEST = 13,
+    ROOMS_GET_SUCCESS = 14,
 
-	JOINED_NEW_GROUP = 15,
+    JOINED_NEW_GROUP = 15,
 
-	SYSTEM_ANNOUNCEMENT = 16,
+    SYSTEM_ANNOUNCEMENT = 16,
 
-	APP_LOGIN_AUTHENTICATE = 17,
-	APP_LOGIN_AUTHENTICATE_SUCCESS = 18,
-	APP_LOGIN_AUTHENTICATE_ERROR = 19,
-	APP_LOGIN_ACCEPT = 20,
-	APP_LOGIN_REJECT = 21,
+    APP_LOGIN_AUTHENTICATE = 17,
+    APP_LOGIN_AUTHENTICATE_SUCCESS = 18,
+    APP_LOGIN_AUTHENTICATE_ERROR = 19,
+    APP_LOGIN_ACCEPT = 20,
+    APP_LOGIN_REJECT = 21,
 
-	GROUP_REMOVED = 22,
-	CHANNEL_REMOVED = 23,
+    GROUP_REMOVED = 22,
+    CHANNEL_REMOVED = 23,
 
-	CHANNEL_CREATE = 30,
-	CHANNEL_DISCONNECT = 31,
-	CHANNEL_UPDATE = 32,
+    CHANNEL_CREATE = 30,
+    CHANNEL_DISCONNECT = 31,
+    CHANNEL_UPDATE = 32,
 
-	PING = 9001
+    PING = 9001
 }
 
 export enum UserFlags {
-	None = 0,
-	Pro = 1 << 0,
-	Dev = 1 << 1,
-	Early = 1 << 2,
-	ClosedBeta = 1 << 3,
-	System = 1 << 4,
-	AppDeveloper = 1 << 5
+    None = 0,
+    Pro = 1 << 0,
+    Dev = 1 << 1,
+    Early = 1 << 2,
+    ClosedBeta = 1 << 3,
+    System = 1 << 4,
+    AppDeveloper = 1 << 5
 }
 
 export enum Permissions {
-	None = 0,
+    None = 0,
 
-	ViewMessages = 1 << 0,
-	SendMessages = 1 << 1,
+    ViewMessages = 1 << 0,
+    SendMessages = 1 << 1,
 
-	BanMembers = 1 << 2,
-	KickMembers = 1 << 3,
+    BanMembers = 1 << 2,
+    KickMembers = 1 << 3,
 
-	ManageGroup = 1 << 4,
-	ManageRoles = 1 << 5,
-	ManageChannels = 1 << 6,
-	ManageMessages = 1 << 7,
-	ManageInvites = 1 << 8,
-	ManageMembers = 1 << 9,
-	ManageSelfMember = 1 << 10,
-	ManageWebhooks = 1 << 11,
+    ManageGroup = 1 << 4,
+    ManageRoles = 1 << 5,
+    ManageChannels = 1 << 6,
+    ManageMessages = 1 << 7,
+    ManageInvites = 1 << 8,
+    ManageMembers = 1 << 9,
+    ManageSelfMember = 1 << 10,
+    ManageWebhooks = 1 << 11,
 
-	ViewChannelHistory = 1 << 12,
-	ViewAuditLog = 1 << 13,
-	CreateInvite = 1 << 14,
-	AttachFiles = 1 << 15,
+    ViewChannelHistory = 1 << 12,
+    ViewAuditLog = 1 << 13,
+    CreateInvite = 1 << 14,
+    AttachFiles = 1 << 15,
 
-	All = ~(~0 << 16)
+    All = ~(~0 << 16)
 }
 
 /* export ??? GatewayRequest {
-	[GatewayEvent.AUTHENTICATE]: {
-		token: string;
-		allRooms?: boolean;
-	};
-	[GatewayEvent.CHANNEL_JOIN]: {
-		id: string;
-	};
-	[GatewayEvent.MESSAGE_SEND]: {
-		content: string;
-		channelID?: string;
-	};
-	[GatewayEvent.GROUP_GET_REQUEST]: Record<string, never>;
-	[GatewayEvent.ROOMS_GET_REQUEST]: {
-		groupID: string;
-	}
-	[GatewayEvent.PING]: Record<string, never>;
+    [GatewayEvent.AUTHENTICATE]: {
+        token: string;
+        allRooms?: boolean;
+    };
+    [GatewayEvent.CHANNEL_JOIN]: {
+        id: string;
+    };
+    [GatewayEvent.MESSAGE_SEND]: {
+        content: string;
+        channelID?: string;
+    };
+    [GatewayEvent.GROUP_GET_REQUEST]: Record<string, never>;
+    [GatewayEvent.ROOMS_GET_REQUEST]: {
+        groupID: string;
+    }
+    [GatewayEvent.PING]: Record<string, never>;
 } */
 
 export interface APIResponse<T> {
@@ -146,14 +146,14 @@ export enum APIError {
     NO_INVITE_CODE,
     INVALID_USERNAME,
     ALREADY_IN_GROUP,
-	NO_SUCH_APP,
-	INVALID_SECRET,
-	NO_SUCH_CHANNEL,
-	NO_SUCH_MEMBER,
-	USER_BANNED,
-	USER_NOT_BANNED,
-	NO_SUCH_ROLE,
-	NO_SUCH_OVERRIDE,
+    NO_SUCH_APP,
+    INVALID_SECRET,
+    NO_SUCH_CHANNEL,
+    NO_SUCH_MEMBER,
+    USER_BANNED,
+    USER_NOT_BANNED,
+    NO_SUCH_ROLE,
+    NO_SUCH_OVERRIDE,
 }
 
 export interface Channel {
@@ -167,43 +167,43 @@ export interface Channel {
 }
 
 export interface Group {
-	id: string;
-	name: string;
-	avatarURL: string;
-	defaultPermissions: Permissions;
-	createdAt: string;
-	updatedAt: string;
-	channels?: Map<string, Channel>;
-	roles?: Map<string, Role>;
+    id: string;
+    name: string;
+    avatarURL: string;
+    defaultPermissions: Permissions;
+    createdAt: string;
+    updatedAt: string;
+    channels?: Map<string, Channel>;
+    roles?: Map<string, Role>;
 }
 
 export interface User {
-	username: string;
-	email: string;
-	displayName: string | null;
-	flags: UserFlags;
-	proSince: string | null;
-	proUntil: string | null;
-	avatarURL: string;
+    username: string;
+    email: string;
+    displayName: string | null;
+    flags: UserFlags;
+    proSince: string | null;
+    proUntil: string | null;
+    avatarURL: string;
 }
 
 export interface Member {
-	username: string; // actual username
-	name: string; // nickname
-	displayName: string; // global displayName
-	avatarURL: string;
+    username: string; // actual username
+    name: string; // nickname
+    displayName: string; // global displayName
+    avatarURL: string;
 }
 
 export interface Message {
-	id: string;
-	author: Member;
-	content: string;
-	timestamp: string;
+    id: string;
+    author: Member;
+    content: string;
+    timestamp: string;
 }
 
 export interface Role {
-	id: string;
-	name: string;
-	groupId: string;
-	permissions?: number;
+    id: string;
+    name: string;
+    groupId: string;
+    permissions?: number;
 }
